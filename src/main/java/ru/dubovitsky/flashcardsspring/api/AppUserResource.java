a5efd383bb691512e427f423c54d75df5a52db59
user <y.dubovitsky@gmail.com>
2021-11-22 14:09:27 +0300

    added basic endpoints: /user/save, /user/addrole, /role/save and etc.

--- src/main/java/ru/dubovitsky/flashcardsspring/api/AppUserResource.java
+++ src/main/java/ru/dubovitsky/flashcardsspring/api/AppUserResource.java
@@ -1,13 +1,15 @@
 package ru.dubovitsky.flashcardsspring.api;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.dubovitsky.flashcardsspring.model.AppUser;
+import ru.dubovitsky.flashcardsspring.model.Role;
 import ru.dubovitsky.flashcardsspring.service.AppUserService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,11 +19,37 @@ public class AppUserResource {
 
     private final AppUserService appUserService;
 
-    @GetMapping("/users")
+    @GetMapping("/user/all")
     public ResponseEntity<List<AppUser>> getAppUsers() {
         return ResponseEntity.ok().body(appUserService.getAppUsers());
     }
 
+    @PostMapping("/user/save")
+    public ResponseEntity<AppUser> saveAppUser(@RequestBody AppUser appUser) {
+        AppUser savedAppUser = appUserService.saveAppUser(appUser);
+        return ResponseEntity.created(getUriForEndpoint("/api/user/save")).body(savedAppUser);
+    }
 
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveAppUser(@RequestBody Role role) {
+        Role savedRole = appUserService.saveRole(role);
+        return ResponseEntity.created(getUriForEndpoint("/api/role/save")).body(savedRole);
+    }
 
+    @PostMapping("/user/addrole")
+    public ResponseEntity<?> addRoleToUser(@RequestBody AppUserWithRoleForm form) {
+        appUserService.addRoleToAppUser(form.username, form.role);
+        return ResponseEntity.ok().build();
+    }
+
+    private URI getUriForEndpoint(String endpoint) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path(endpoint).toUriString());
+        return uri;
+    }
+}
+
+@Data
+class AppUserWithRoleForm { //TODO Фасад или Request?
+    String username;
+    String role;
 }
