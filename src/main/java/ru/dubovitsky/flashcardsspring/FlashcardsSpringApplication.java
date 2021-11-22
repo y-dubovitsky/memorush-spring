6be7b7c419d4312e7c16cb4cdcf979b876bd4b65
user <y.dubovitsky@gmail.com>
2021-11-22 14:27:37 +0300

    added CommandLineRunner to init some users and roles

--- src/main/java/ru/dubovitsky/flashcardsspring/FlashcardsSpringApplication.java
+++ src/main/java/ru/dubovitsky/flashcardsspring/FlashcardsSpringApplication.java
@@ -1,13 +1,39 @@
 package ru.dubovitsky.flashcardsspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.dubovitsky.flashcardsspring.model.AppUser;
+import ru.dubovitsky.flashcardsspring.model.Role;
+import ru.dubovitsky.flashcardsspring.service.AppUserService;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class FlashcardsSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlashcardsSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(AppUserService appUserService) { //Запускается, когда стартует приложение
+		return args -> {
+			appUserService.saveRole(new Role(null, "ROLE_ADMIN"));
+			appUserService.saveRole(new Role(null, "ROLE_USER"));
+			appUserService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			appUserService.saveAppUser(new AppUser(null, "admin", "admin", new ArrayList<>()));
+			appUserService.saveAppUser(new AppUser(null, "user", "user", new ArrayList<>()));
+			appUserService.saveAppUser(new AppUser(null, "a", "a", new ArrayList<>()));
+
+			appUserService.addRoleToAppUser("admin", "ROLE_ADMIN");
+			appUserService.addRoleToAppUser("user", "ROLE_USER");
+			appUserService.addRoleToAppUser("a", "ROLE_SUPER_ADMIN");
+			appUserService.addRoleToAppUser("a", "ROLE_ADMIN");
+			appUserService.addRoleToAppUser("a", "ROLE_USER");
+		};
 	}
 
 }
