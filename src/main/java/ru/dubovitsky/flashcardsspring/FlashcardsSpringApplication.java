0d8a515167adbe8459a04ddcc1b1092260432cf6
user <y.dubovitsky@gmail.com>
2021-11-22 11:56:46 +0300

    init commit

---
+++ src/main/java/ru/dubovitsky/flashcardsspring/FlashcardsSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.dubovitsky.flashcardsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dubovitsky/flashcardsspring/FlashcardsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dubovitsky.flashcardsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
