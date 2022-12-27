cd99af7df3ed4ba5947f8f28567ad46f2ad3ab29
user <y.dubovitsky@gmail.com>
2022-12-27 14:01:08 +0300

    Unused service and repo deleted

--- src/main/java/ru/dubovitsky/flashcardsspring/repository/CardPagingAndSortingRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.dubovitsky.flashcardsspring.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import ru.dubovitsky.flashcardsspring.model.Card;
-
-public interface CardPagingAndSortingRepository extends PagingAndSortingRepository<Card, Long> {
-}
