6387079c48474195645833590753d9788ac353bf
user <y.dubovitsky@gmail.com>
2023-06-05 14:23:35 +0300

    Empty test with error removed

--- src/test/java/ru/dubovitsky/memorush/MemorushApplicationTests.java
+++ src/test/java/ru/dubovitsky/memorush/MemorushApplicationTests.java
@@ -1,13 +1,9 @@
 package ru.dubovitsky.memorush;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MemorushApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
