a58e2ab36b68704a7114f42b5baa48054ea786de
user <y.dubovitsky@gmail.com>
2023-10-17 11:17:34 +0300

    CORS Policy updated

--- src/main/java/ru/dubovitsky/memorush/security/config/CORSConfig.java
+++ src/main/java/ru/dubovitsky/memorush/security/config/CORSConfig.java
@@ -1,22 +1,29 @@
 package ru.dubovitsky.memorush.security.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import ru.dubovitsky.memorush.config.VariablesConfig;
 
+@Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class CORSConfig {
+
+    private final VariablesConfig variablesConfig;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
-
-
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins(variablesConfig.getAllowedOriginsArray().split(","));
             }
         };
     }
