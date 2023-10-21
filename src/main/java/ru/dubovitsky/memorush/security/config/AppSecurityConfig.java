fc662271e8d3e767097f07a773b8313238d4b32d
y-dubovitsky <pXmJ5M9dvP>
2023-10-21 11:59:45 +0300

    CORS config moved to AppSecurityConfig

--- src/main/java/ru/dubovitsky/memorush/security/config/AppSecurityConfig.java
+++ src/main/java/ru/dubovitsky/memorush/security/config/AppSecurityConfig.java
@@ -1,20 +1,26 @@
 package ru.dubovitsky.memorush.security.config;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import ru.dubovitsky.memorush.config.VariablesConfig;
 import ru.dubovitsky.memorush.security.filter.JwtTokenVerifierFilter;
 import ru.dubovitsky.memorush.security.filter.JwtUsernameAndPasswordAuthFilter;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     private final JwtConfig jwtConfig;
 
@@ -26,6 +32,22 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         //! Проблема в том, что эти два фильтра не управляются спрингом, т.к. мы создали их через new()
         http.addFilter(new JwtUsernameAndPasswordAuthFilter(authenticationManager(), jwtConfig));
         http.addFilterAfter(new JwtTokenVerifierFilter(jwtConfig), JwtUsernameAndPasswordAuthFilter.class);
+    }
+
+    private final VariablesConfig variablesConfig;
+
+    //https://sysout.ru/nastrojka-cors-v-spring-security/
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        log.info(String.format("CORS AllowedOrigins %s", variablesConfig.getAllowedOriginsArray()));
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(variablesConfig.getAllowedOriginsArray())
+                        .allowedMethods("*");
+            }
+        };
     }
 
 }
